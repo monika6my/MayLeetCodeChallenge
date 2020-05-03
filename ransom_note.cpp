3b220511232963b60fa953bf4de1265e4bfdeb73
monika6my <monika6my@gmail.com>
2020-05-03 21:34:18 +0530

    Day 3 - Ransom Note solution with comments

--- ransom_note.cpp
+++ ransom_note.cpp
@@ -1,14 +1,22 @@
 bool canConstruct(string ransomNote, string magazine) {
         
-        /*
-        sort(string, string+ len);*/
         int len = magazine.length();
+        int r_len = ransomNote.length();
+
+        // create an unordered map letterCount
+        // key: letters in magazine
+        // value: occurences of a letter in magazine
         unordered_map<char, int> letterCount;
         
+
+        // update the occurences of each letter as we iterate through the string magazine
         for(int i =0; i<len; i++)
             letterCount[magazine[i]]++;
  
-        for(int i = 0; i<ransomNote.length(); i++)
+        // for each letter in ransom note, check if it is available in letterCount
+        // if available, the count of letter in letterCount is decremented by 1
+        // if not available return false
+        for(int i = 0; i<r_len; i++)
         {
             unordered_map<char, int>::iterator x = letterCount.find(ransomNote[i]);
             if(x!=letterCount.end()&&(x->second>0))
